da27c3f00a9310254a68823186037ec64c8d1a79
Ditel Segup <ditel@MacBook-Pro-de-Ditel.local>
2019-10-07 18:49:17 -0300

    home_filtro_nao_parceiro_db

--- model/database/maindb.sql
+++ model/database/maindb.sql
@@ -1,23 +1,30 @@
--- --------------------------------------------------------
--- Servidor:                     127.0.0.1
--- Versão do servidor:           10.3.15-MariaDB - mariadb.org binary distribution
--- OS do Servidor:               Win64
--- HeidiSQL Versão:              9.5.0.5196
--- --------------------------------------------------------
+CREATE DATABASE  IF NOT EXISTS `maindb` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `maindb`;
+-- MySQL dump 10.13  Distrib 5.7.17, for macos10.12 (x86_64)
+--
+-- Host: 127.0.0.1    Database: maindb
+-- ------------------------------------------------------
+-- Server version	5.6.37
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+--
+-- Table structure for table `functionality`
+--
 
--- Copiando estrutura do banco de dados para maindb
-CREATE DATABASE IF NOT EXISTS `maindb` /*!40100 DEFAULT CHARACTER SET utf8 */;
-USE `maindb`;
-
--- Copiando estrutura para tabela maindb.functionality
-CREATE TABLE IF NOT EXISTS `functionality` (
+DROP TABLE IF EXISTS `functionality`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `functionality` (
   `id` int(11) NOT NULL,
   `idsprint` int(11) DEFAULT NULL,
   `name` varchar(45) DEFAULT NULL,
@@ -27,13 +34,25 @@ CREATE TABLE IF NOT EXISTS `functionality` (
   PRIMARY KEY (`id`),
   KEY `fk_idsprint_idx` (`idsprint`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- Copiando dados para a tabela maindb.functionality: ~0 rows (aproximadamente)
+--
+-- Dumping data for table `functionality`
+--
+
+LOCK TABLES `functionality` WRITE;
 /*!40000 ALTER TABLE `functionality` DISABLE KEYS */;
 /*!40000 ALTER TABLE `functionality` ENABLE KEYS */;
+UNLOCK TABLES;
 
--- Copiando estrutura para tabela maindb.mission
-CREATE TABLE IF NOT EXISTS `mission` (
+--
+-- Table structure for table `mission`
+--
+
+DROP TABLE IF EXISTS `mission`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `mission` (
   `id` int(11) NOT NULL,
   `idphase` int(11) DEFAULT NULL,
   `name` varchar(45) DEFAULT NULL,
@@ -41,13 +60,50 @@ CREATE TABLE IF NOT EXISTS `mission` (
   PRIMARY KEY (`id`),
   KEY `fk_idphase_idx` (`idphase`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- Copiando dados para a tabela maindb.mission: ~0 rows (aproximadamente)
+--
+-- Dumping data for table `mission`
+--
+
+LOCK TABLES `mission` WRITE;
 /*!40000 ALTER TABLE `mission` DISABLE KEYS */;
 /*!40000 ALTER TABLE `mission` ENABLE KEYS */;
+UNLOCK TABLES;
 
--- Copiando estrutura para tabela maindb.phase
-CREATE TABLE IF NOT EXISTS `phase` (
+--
+-- Table structure for table `partner`
+--
+
+DROP TABLE IF EXISTS `partner`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `partner` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_user` int(11) NOT NULL,
+  `id_partner` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `partner`
+--
+
+LOCK TABLES `partner` WRITE;
+/*!40000 ALTER TABLE `partner` DISABLE KEYS */;
+INSERT INTO `partner` VALUES (1,1,2),(2,1,3),(3,1,4),(4,1,5),(5,1,6),(6,1,7);
+/*!40000 ALTER TABLE `partner` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `phase`
+--
+
+DROP TABLE IF EXISTS `phase`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `phase` (
   `id` int(11) NOT NULL,
   `idproject` int(11) DEFAULT NULL,
   `name` varchar(45) DEFAULT NULL,
@@ -55,32 +111,50 @@ CREATE TABLE IF NOT EXISTS `phase` (
   PRIMARY KEY (`id`),
   KEY `fk_idproject_idx` (`idproject`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- Copiando dados para a tabela maindb.phase: ~0 rows (aproximadamente)
+--
+-- Dumping data for table `phase`
+--
+
+LOCK TABLES `phase` WRITE;
 /*!40000 ALTER TABLE `phase` DISABLE KEYS */;
 /*!40000 ALTER TABLE `phase` ENABLE KEYS */;
+UNLOCK TABLES;
 
--- Copiando estrutura para tabela maindb.project
-CREATE TABLE IF NOT EXISTS `project` (
+--
+-- Table structure for table `project`
+--
+
+DROP TABLE IF EXISTS `project`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `project` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` text DEFAULT NULL,
-  `method` text DEFAULT NULL,
+  `name` text,
+  `method` text,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- Copiando dados para a tabela maindb.project: ~1 rows (aproximadamente)
+--
+-- Dumping data for table `project`
+--
+
+LOCK TABLES `project` WRITE;
 /*!40000 ALTER TABLE `project` DISABLE KEYS */;
-INSERT INTO `project` (`id`, `name`, `method`) VALUES
-	(1, 'Elias Nascido do Ferro', 'cascata'),
-	(2, 'Lilly is Unniverse', 'espiral'),
-	(3, 'Jogo da Velha visualg', 'prototipacao'),
-	(4, 'bunnyrunner', 'incremental'),
-	(5, 'asteroids', 'scrum'),
-	(6, 'Hallo', 'scrum');
+INSERT INTO `project` VALUES (1,'Elias Nascido do Ferro','cascata'),(2,'Lilly is Unniverse','espiral'),(3,'Jogo da Velha visualg','prototipacao'),(4,'bunnyrunner','incremental'),(5,'asteroids','scrum'),(6,'Hallo','scrum');
 /*!40000 ALTER TABLE `project` ENABLE KEYS */;
+UNLOCK TABLES;
 
--- Copiando estrutura para tabela maindb.scrum
-CREATE TABLE IF NOT EXISTS `scrum` (
+--
+-- Table structure for table `scrum`
+--
+
+DROP TABLE IF EXISTS `scrum`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `scrum` (
   `id` int(11) NOT NULL,
   `idproject` int(11) DEFAULT NULL,
   `product_owner` varchar(45) DEFAULT NULL,
@@ -89,50 +163,77 @@ CREATE TABLE IF NOT EXISTS `scrum` (
   PRIMARY KEY (`id`),
   KEY `fk_ìdproject_idx` (`idproject`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- Copiando dados para a tabela maindb.scrum: ~0 rows (aproximadamente)
+--
+-- Dumping data for table `scrum`
+--
+
+LOCK TABLES `scrum` WRITE;
 /*!40000 ALTER TABLE `scrum` DISABLE KEYS */;
 /*!40000 ALTER TABLE `scrum` ENABLE KEYS */;
+UNLOCK TABLES;
 
--- Copiando estrutura para tabela maindb.sprint
-CREATE TABLE IF NOT EXISTS `sprint` (
+--
+-- Table structure for table `sprint`
+--
+
+DROP TABLE IF EXISTS `sprint`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sprint` (
   `id` int(11) NOT NULL,
   `idscrum` int(11) DEFAULT NULL,
   `time` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_idscrum_idx` (`idscrum`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- Copiando dados para a tabela maindb.sprint: ~0 rows (aproximadamente)
+--
+-- Dumping data for table `sprint`
+--
+
+LOCK TABLES `sprint` WRITE;
 /*!40000 ALTER TABLE `sprint` DISABLE KEYS */;
 /*!40000 ALTER TABLE `sprint` ENABLE KEYS */;
+UNLOCK TABLES;
 
--- Copiando estrutura para tabela maindb.user
-CREATE TABLE IF NOT EXISTS `user` (
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` text DEFAULT NULL,
-  `login` text DEFAULT NULL,
-  `password` text DEFAULT NULL,
-  `number_phone` int(11) DEFAULT 0,
-  `email` text DEFAULT '0',
-  `partner` int(11) DEFAULT NULL,
+  `name` text,
+  `login` text,
+  `password` text,
+  `number_phone` int(11) DEFAULT '0',
+  `email` text,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- Copiando dados para a tabela maindb.user: ~8 rows (aproximadamente)
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` (`id`, `name`, `login`, `password`, `number_phone`, `email`, `partner`) VALUES
-	(1, 'HENRICK DE SOUSA NOGUEIRA', 'henrick@123', '8ce4b9070698b32a73a3d82413497359', 980877929, 'ricknogueira1231@gmail.com', NULL),
-	(2, 'Igor de Sousa Nogueira', 'igor@123', 'bd487cf43f6af956a6e9842ac8deeac5', 111111111, 'igor.nogueira@gmail.com', NULL),
-	(3, 'Wanessa Pinto Campos', 'wanessa@123', 'f22a5be545ff0aa9de0169b08f9d3ae4', 222222222, 'wanessacampos1997@gmail.com', NULL),
-	(4, 'Admin', 'admin@admin.com', '21232f297a57a5a743894a0e4a801fc3', 333333333, 'admin@admin.com', NULL),
-	(5, 'Odiney Nogueira', 'odiney@nogueira.com', '3728bc7f0eb528cd759b1608f6dd8a6c', 919592934, 'odiney.nogueira@hotmail.com', NULL),
-	(6, 'Nielton Brito', 'nielton@email.com', '286f32e353ffd1668dde127e2c5c0e27', 5555555, 'nielton.brito@gmail.com', NULL),
-	(7, 'Teste1', 'teste@1', '9128523da3369445833e0dbc0e464a00', 666666, 'teste@senhaaparece', NULL),
-	(8, 'Teste2', 'teste@2', '9128523da3369445833e0dbc0e464a00', 7777777, 'teste@senhaaparece2', NULL),
-	(9, 'Teste3', 'teste@3', '9128523da3369445833e0dbc0e464a00', 8888888, 'teste@senhaaparece3', NULL);
+INSERT INTO `user` VALUES (1,'HENRICK DE SOUSA NOGUEIRA','henrick@123','8ce4b9070698b32a73a3d82413497359',980877929,'ricknogueira1231@gmail.com'),(2,'Igor de Sousa Nogueira','igor@123','bd487cf43f6af956a6e9842ac8deeac5',111111111,'igor.nogueira@gmail.com'),(3,'Wanessa Pinto Campos','wanessa@123','f22a5be545ff0aa9de0169b08f9d3ae4',222222222,'wanessacampos1997@gmail.com'),(4,'Admin','admin@admin.com','21232f297a57a5a743894a0e4a801fc3',333333333,'admin@admin.com'),(5,'Odiney Nogueira','odiney@nogueira.com','3728bc7f0eb528cd759b1608f6dd8a6c',919592934,'odiney.nogueira@hotmail.com'),(6,'Nielton Brito','nielton@email.com','286f32e353ffd1668dde127e2c5c0e27',5555555,'nielton.brito@gmail.com'),(7,'Teste1','teste@1','9128523da3369445833e0dbc0e464a00',666666,'teste@senhaaparece'),(8,'Teste2','teste@2','9128523da3369445833e0dbc0e464a00',7777777,'teste@senhaaparece2'),(9,'Teste3','teste@3','9128523da3369445833e0dbc0e464a00',8888888,'teste@senhaaparece3');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-10-07 18:46:24
