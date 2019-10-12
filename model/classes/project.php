8e034f1a387e517ff0db0632044bc0b83fa57e62
Ditel Segup <ditel@MacBook-Pro-de-Ditel.local>
2019-10-11 22:08:59 -0300

    ínsert_project_front

--- model/classes/project.php
+++ model/classes/project.php
@@ -1,13 +1,15 @@
 <?php
 class project{
-//public $iduser;
-//public $partners;
+public $iduser;
+public $partners;
 public $name;
 public $method;
 	/** 
     construtor
     **/
-	function __construct($name, $method){
+	function __construct($iduser, $partners, $name, $method){
+		$this->iduser = $iduser;
+		$this->partners = $partners;
 		$this->name = $name;
 		$this->method = $method;
 	}
